684b7022d8e7101311415cef23f42a33a2c80285
Ksenia Liubynska <ksenia.liubynska@sigma.software>
2020-05-04 17:37:17 +0300

    Backend Project Skeleton

---
+++ backend/DocuSign.MyHR/Controllers/InfoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DocuSign.MyHR.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Ping")]
+        public string Ping()
+        {
+            return "OK";
+        }
+    }
+}
