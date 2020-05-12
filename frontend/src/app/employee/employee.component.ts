0b550f71cdc446770cf2621c3cc97ac9f2ab9576
Pavliuchenkova <ahnesa.pavliuchenkova@sigma.software>
2020-05-12 12:06:12 +0300

    Conponents for teh Employee page

--- frontend/src/app/employee/employee.component.ts
+++ frontend/src/app/employee/employee.component.ts
@@ -1,15 +1,15 @@
+import { EmployeeService } from './employee.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
+    this.employeeService.getUser();
   }
-
 }
--- frontend/src/app/employee/employee.module.ts
+++ frontend/src/app/employee/employee.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { EmployeeComponent } from './employee.component';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { ProfileComponent } from './profile/profile.component';
+import { ActionsComponent } from './actions/actions.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [EmployeeComponent],
-  imports: [CommonModule, EmployeeRoutingModule, TranslateModule.forChild()],
+  declarations: [EmployeeComponent, ProfileComponent, ActionsComponent],
+  imports: [
+    CommonModule,
+    EmployeeRoutingModule,
+    TranslateModule.forChild(),
+    FormsModule,
+  ],
 })
 export class EmployeeModule {}
