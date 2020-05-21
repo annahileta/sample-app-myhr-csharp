c9603ae001332a1ed651357ac71a6d77bcff2150
Pavliuchenkova <ahnesa.pavliuchenkova@sigma.software>
2020-05-21 11:28:22 +0300

    Deleted timecard component

--- DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/employee/employee-routing.module.ts
+++ DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/employee/employee-routing.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { EmployeeComponent } from "./employee.component";
-import { TimeCardComponent } from "./actions/timecard/timecard.component";
 
-const routes: Routes = [
-  { path: "", component: EmployeeComponent },
-  {
-    path: "timecard",
-    component: TimeCardComponent,
-  },
-];
+const routes: Routes = [{ path: "", component: EmployeeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
