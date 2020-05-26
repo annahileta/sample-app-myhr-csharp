6965389c89a83605ad4a2cd7694b3cf9e5f228be
Pavliuchenkova <ahnesa.pavliuchenkova@sigma.software>
2020-05-26 11:02:47 +0300

    Unit tests: AppComponent

--- DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/app.component.ts
+++ DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { environment } from "src/environments/environment";
-import { AuthenticationService } from "./core/authentication/auth.service";
 
 @Component({
   selector: "app-root",
@@ -10,11 +8,7 @@ import { AuthenticationService } from "./core/authentication/auth.service";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  constructor(
-    private translateService: TranslateService,
-    private authenticationService: AuthenticationService,
-    private router: Router
-  ) {}
+  constructor(private translateService: TranslateService) {}
   ngOnInit(): void {
     this.translateService.use(environment.defaultLanguage);
   }
