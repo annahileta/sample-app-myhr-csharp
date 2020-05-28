eb86e289720d8669556aa345eea635a92fe73300
Pavliuchenkova <ahnesa.pavliuchenkova@sigma.software>
2020-05-28 16:19:54 +0300

    Removed AboutComponent

--- DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/app-routing.module.ts
+++ DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/app-routing.module.ts
@@ -1,30 +1,25 @@
-import { NgModule } from '@angular/core'
-import { Routes, RouterModule } from '@angular/router'
-import { AboutComponent } from './about/about.component'
-import { AuthGuard } from './core/authentication/auth.guard'
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./core/authentication/auth.guard";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    path: "",
+    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
     canLoad: [AuthGuard],
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
   {
-    path: 'employee',
+    path: "employee",
     loadChildren: () =>
-      import('./employee/employee.module').then((m) => m.EmployeeModule),
+      import("./employee/employee.module").then((m) => m.EmployeeModule),
     canLoad: [AuthGuard],
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
-  {
-    path: 'about',
-    component: AboutComponent
-  }
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
