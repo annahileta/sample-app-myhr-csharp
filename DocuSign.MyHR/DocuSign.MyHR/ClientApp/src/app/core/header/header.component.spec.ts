d6208b7eaab8bf4f826de66df8566298c00b62f8
Pavliuchenkova <ahnesa.pavliuchenkova@sigma.software>
2020-05-29 13:26:34 +0300

    Header styling

--- DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/core/header/header.component.spec.ts
+++ DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/core/header/header.component.spec.ts
@@ -1,44 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { HeaderComponent } from './header.component'
-import { AuthenticationService } from '../authentication/auth.service'
+import { HeaderComponent } from "./header.component";
+import { AuthenticationService } from "../authentication/auth.service";
+import { RouterTestingModule } from "@angular/router/testing";
 
 class AuthenticationServiceStub {
-  public logout () {}
+  public logout() { }
 }
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent
-  let fixture: ComponentFixture<HeaderComponent>
-  let authenticationService: AuthenticationService
+describe("HeaderComponent", () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+  let authenticationService: AuthenticationService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [HeaderComponent],
       providers: [
-        { provide: AuthenticationService, useClass: AuthenticationServiceStub }
-      ]
-    }).compileComponents()
-  }))
+        { provide: AuthenticationService, useClass: AuthenticationServiceStub },
+      ],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent)
-    component = fixture.componentInstance
-    authenticationService = TestBed.inject(AuthenticationService)
-    spyOn(authenticationService, 'logout').and.stub()
-    fixture.detectChanges()
-  })
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    authenticationService = TestBed.inject(AuthenticationService);
+    spyOn(authenticationService, "logout").and.stub();
+    fixture.detectChanges();
+  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
 
-  describe('logout', () => {
-    it('should call logout method from authentication service', () => {
+  describe("logout", () => {
+    it("should call logout method from authentication service", () => {
       // act
-      component.logout()
+      component.logout();
       // assert
-      expect(authenticationService.logout).toHaveBeenCalled()
-    })
-  })
-})
+      expect(authenticationService.logout).toHaveBeenCalled();
+    });
+  });
+});
--- DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/core/header/header.component.ts
+++ DocuSign.MyHR/DocuSign.MyHR/ClientApp/src/app/core/header/header.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core'
-import { AuthenticationService } from '../authentication/auth.service'
+import { Observable } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../authentication/auth.service";
+import { Router, NavigationEnd } from "@angular/router";
+import { map, filter, startWith } from "rxjs/operators";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
-  constructor (private authenticationService: AuthenticationService) {}
+  isHomePage$: Observable<boolean>;
 
-  ngOnInit (): void {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {}
 
-  logout () {
-    this.authenticationService.logout()
+  ngOnInit(): void {
+    this.isHomePage$ = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => event.url === "/"),
+      startWith(true)
+    );
+  }
+
+  logout() {
+    this.authenticationService.logout();
   }
 }
