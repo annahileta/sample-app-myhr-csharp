834c5a473e0fb86c1caa8b9c2218a803d4ed36be
Ksenia Liubynska <ksenia.liubynska@sigma.software>
2020-06-03 18:13:03 +0300

    Add temporary logging

--- DocuSign.MyHR/DocuSign.MyHR/Security/AuthenticationService.cs
+++ DocuSign.MyHR/DocuSign.MyHR/Security/AuthenticationService.cs
@@ -28,16 +28,27 @@ namespace DocuSign.MyHR.Security
 
         public (ClaimsPrincipal, AuthenticationProperties) AuthenticateFromJwt()
         {
-            OAuth.OAuthToken authToken = _apiClient.RequestJWTUserToken(
-                _configurationService["DocuSign:IntegrationKey"],
-                _configurationService["DocuSign:UserId"],
-                _configurationService["DocuSign:AuthServer"],
-                Convert.FromBase64String(_configurationService["DocuSign:RSAPrivateKey"]),
-                1,
-                new List<string> { "click.manage", "signature" });
+            {
+                OAuth.OAuthToken authToken;
+                try
+                {
+                    authToken = _apiClient.RequestJWTUserToken(
+                         _configurationService["DocuSign:IntegrationKey"],
+                         _configurationService["DocuSign:UserId"],
+                         _configurationService["DocuSign:AuthServer"],
+                         Convert.FromBase64String(_configurationService["DocuSign:RSAPrivateKey"]),
+                         1,
+                         new List<string> { "click.manage", "signature" });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw new Exception($"Key: { _configurationService["DocuSign:IntegrationKey"]}, User:{  _configurationService["DocuSign:UserId"]} , server : { _configurationService["DocuSign:AuthServer"]}, privateKey : { _configurationService["DocuSign:RSAPrivateKey"]}", e);
+                }
 
-            OAuth.UserInfo userInfo = _apiClient.GetUserInfo(authToken.access_token);
-            var claims = new List<Claim>
+
+                OAuth.UserInfo userInfo = _apiClient.GetUserInfo(authToken.access_token);
+                var claims = new List<Claim>
             {
                 new Claim("access_token", authToken.access_token),
                 new Claim(ClaimTypes.NameIdentifier, userInfo.Sub),
@@ -46,23 +57,23 @@ namespace DocuSign.MyHR.Security
                 new Claim("authType", LoginType.JWT.ToString()),
             };
 
-            foreach (var account in userInfo.Accounts)
-            {
-                claims.Add(new Claim("accounts", JsonConvert.SerializeObject(account)));
+                foreach (var account in userInfo.Accounts)
+                {
+                    claims.Add(new Claim("accounts", JsonConvert.SerializeObject(account)));
+                }
+
+                var claimsIdentity = new ClaimsIdentity(
+                    claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    ExpiresUtc = DateTimeOffset.Now.AddDays(1),
+                    IsPersistent = true,
+                };
+
+                return (new ClaimsPrincipal(claimsIdentity), authProperties);
             }
-
-            var claimsIdentity = new ClaimsIdentity(
-                claims,
-                CookieAuthenticationDefaults.AuthenticationScheme);
-
-            var authProperties = new AuthenticationProperties
-            {
-                AllowRefresh = true,
-                ExpiresUtc = DateTimeOffset.Now.AddDays(1),
-                IsPersistent = true,
-            };
-
-            return (new ClaimsPrincipal(claimsIdentity), authProperties);
         }
     }
-}
